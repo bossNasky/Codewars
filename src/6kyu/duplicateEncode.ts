d28a0f87b955ac9e74eef12ceb760aa44be3a2d5
bossNasky <naskret.kamil@wp.pl>
2023-02-24 15:35:58 +0100

    6kyu challenge duplicateEncode solution

---
+++ src/6kyu/duplicateEncode.ts
@@ -0,0 +1,11 @@
+export function duplicateEncode(word: string) {
+  const lowerCaseWord = word.toLowerCase();
+  const encoder = lowerCaseWord
+    .split("")
+    .map((char) =>
+      lowerCaseWord.indexOf(char) === lowerCaseWord.lastIndexOf(char)
+        ? ")"
+        : "("
+    );
+  return encoder.join("");
+}
